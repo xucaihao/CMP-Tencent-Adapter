1e607fc743362c868e132ecd49079a65c9935691
xucaihao <467046520@qq.com>
2018-04-10 11:32:15 +0800

    搭建项目

---
+++ src/main/java/com/cmp/tencentadapter/TencentadapterApplication.java
@@ -0,0 +1,12 @@
+package com.cmp.tencentadapter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TencentadapterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TencentadapterApplication.class, args);
+	}
+}
